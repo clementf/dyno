060198c9576489860b08c4168524514751bb84e4
Clement Ferey <clement.ferey@gmail.com>
2019-02-17 22:30:09 +0100

    Host configuration (fix for links in mailers)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# used to have correct host when generating full links
+Dyno::Application.default_url_options = Dyno::Application.config.action_mailer.default_url_options
