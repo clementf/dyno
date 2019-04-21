a27777382f8193ca545e4e7015f422dbae51477f
Clement Ferey <clement.ferey@gmail.com>
2019-04-21 22:00:31 +0200

    When creating blocks, put them in requested state

--- app/services/lesson_factory.rb
+++ app/services/lesson_factory.rb
@@ -15,7 +15,7 @@ module LessonFactory
       lesson = Lesson.create!(user: user, target_language: langs.target_language)
 
       Sentence.ready_for_lesson(limit: block_count).each do |sentence|
-        Block.create!(lesson: lesson, sentence: sentence)
+        Block.create!(lesson: lesson, sentence: sentence, state: 'requested')
       end
 
       raise ActiveRecord::Rollback if lesson.blocks.empty?
